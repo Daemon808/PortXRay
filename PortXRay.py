bffdbdc75c4f9c19c6fcce02491a3b60ff123e42
Hugo <Hugoramand@gmail.com>
2024-08-22 18:19:30 +0200

    target ip

--- PortXRay.py
+++ PortXRay.py
@@ -4,3 +4,7 @@ import subprocess
 from datetime import datetime
 
 subprocess.call("clear", shell=True)
+
+target_server_ip = input("Please enter target ip :")
+
+print("Scanne in progress for " + target_server_ip)
